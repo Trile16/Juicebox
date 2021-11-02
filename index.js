2ef462992016f316f9828f44612fc95ff10b5619
Tri <16trile@gmail.com>
2021-11-02 14:38:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// inside db/index.js
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+      FROM users;
+    `
+  );
+
+  return rows;
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+};
