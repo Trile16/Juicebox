6f6ab3a58cb8200af58b190fe367834644e19d00
Tri <16trile@gmail.com>
2021-11-08 13:51:44 -0600

    part 3 of part 2 jb complete

--- api/tags.js
+++ api/tags.js
@@ -6,13 +6,27 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
   const tags = await getAllTags();
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+  try {
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+    const tagged = await getPostsByTagName(tagName);
+    res.send({ tagged });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
