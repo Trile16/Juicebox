f3b24c22b1699bbd447bd6f2d3327947252320f6
Tri <16trile@gmail.com>
2021-11-08 12:48:03 -0600

    Part 2 Complete

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // inside index.js
-const PORT = 3000;
+require("dotenv").config();
+
 const express = require("express");
-const server = express();
-
-const { client } = require("./db");
-client.connect();
-
-server.listen(PORT, () => {
-  console.log("The server is up on port", PORT);
-});
-
-const apiRouter = require("./api");
-server.use("/api", apiRouter);
-
 const morgan = require("morgan");
-server.use(morgan("dev"));
+const PORT = 3000;
+const server = express();
 
 server.use(express.json());
 
+server.use(morgan("dev"));
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
   console.log(req.body);
   console.log("<_____Body Logger END_____>");
 
   next();
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const { client } = require("./db");
+client.connect();
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
 });
